6d53a6d28b3daff0cf04c94f254c9f088e86f3cf
Benjamin Spriggs <benjspriggs@sprico.com>
2016-04-06 18:11:25 -0700

    Add script path for later use in linting

--- lib/loop-control.py
+++ lib/loop-control.py
@@ -18,6 +18,7 @@ def offense(lineno, offense, line):
     if offense is None:
         offense = "Offense"
     print(offense + " at line " + str(lineno) + ": \"" + line + "\"")
+    return 1
 
 # checks a filename for any offenses, prints out a statement at completion
 # returns the number of offenses in the file
@@ -30,13 +31,14 @@ def lint(fn):
         in_class = False
         in_comment = False
         for lineno, line in enumerate(inF.read().splitlines()):
+            # TODO add more style requirements
             if has(forever_while, line):
                 offense(lineno, "Illegal while loop", line)
                 global_offense += 1
             if in_class and has(static_var, line):
                 offense(lineno, "Static member detected", line)
                 global_offense += 1
-            if has(class_declaration, line):
+            if has(class_declaration, line): # TODO have this check the rest of a class's definition instead of setting a boolean flag
                 in_class = True
     if global_offense > 0:
         print("There were " + str(global_offense) + " offenses in '" + fn + "'")
