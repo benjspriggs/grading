fca793a53bdb0d44e3bcc09b66b00f0f00e3884b
Benjamin Spriggs <benjspriggs@sprico.com>
2016-04-07 17:11:21 -0700

    Add cs202_style python

--- lib/cs202_style.py
+++ lib/cs202_style.py
@@ -1,7 +1,9 @@
 # cs202-style.py
 # Stylechecks a bunch of files based on the cs202 style
 
-import loop-control
+import loop_control, sys, getopt
 
 if __name__ == "__main__":
     print("Hello World!")
+    for each in sys.argv[1:]:
+        loop_control.lint(each)
