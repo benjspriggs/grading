295416a14fba60463c177cb29aca49ea7a916637
Benjamin Spriggs <benjspriggs@sprico.com>
2016-04-11 09:52:57 -0700

    Add reporting method to LoopControl, add something else

--- lib/loop_control.py
+++ lib/loop_control.py
@@ -5,12 +5,19 @@
 from lint import *
 
 class LoopControlLinter(Linter):
+    forever_while = re.compile(r"while\s?\((-?[1-9]+|true)?\)")
     def initialize(self):
         self.offenses = 0
 
     def lint(self, fn):
-        return is_lintable(fn)
+        if is_lintable(fn):
+            with open(fn, 'r') as inF:
+                for line in inF.read().splitlines():
+                    if has(forever_while, line):
+                        self.offenses.append((lineno, line, fn))
 
     def offenses(self):
-        return self.offenses
+        return len(self.offenses)
 
+    def report(self):
+        print("There were " + str(offenses()) + " in the files.")
