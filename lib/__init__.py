b74f4e6b13c64f50a5b3678f28f3fbb8a10ea733
Benjamin Spriggs <benjspriggs@sprico.com>
2016-04-11 11:21:38 -0700

    Remove setup

--- lib/__init__.py
+++
@@ -1,1 +0,0 @@
-__all__ = ['lint', 'linter', 'loop_control']
--- lib/test/fixtures/has-forever-while.cpp
+++
@@ -1,8 +0,0 @@
-// has-forever-while.cpp
-
-int main(void)
-{
-	while(true); // this should bring up an offense
-	// while(1); this shouldn't
-	return 0;
-}
