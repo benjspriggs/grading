8b5675b7a301d8abbbdfdee958a104bada8f8543
Benjamin Spriggs <benjspriggs@sprico.com>
2016-04-12 16:31:04 -0700

    Change to comprehension, and count lines of file

--- lint/test/test_loop_control.py
+++ lint/test/test_loop_control.py
@@ -8,7 +8,8 @@ class LoopControlLinterTest(TestCase):
     has_while_file = "test/fixtures/has-forever-while.cpp"
 
     def test_lint(self):
-        self.assertEquals(len(open(self.has_while_file, 'r').read().splitlines()), LoopControlLinter().lint(self.has_while_file))
+        self.assertEquals(sum(1 for line in open(self.has_while_file, 'rb')),
+                LoopControlLinter().lint(self.has_while_file))
 
     def test_offenses(self):
         l = LoopControlLinter()
