753760b1adeca64dbca0cd178060f24b163d4671
Benjamin Spriggs <benjspriggs@sprico.com>
2016-04-12 17:21:23 -0700

    Change assertion in test

--- lint/test/test_loop_control.py
+++ lint/test/test_loop_control.py
@@ -6,9 +6,10 @@ from lib import LoopControlLinter
 
 class LoopControlLinterTest(TestCase):
     has_while_file = "test/fixtures/has-forever-while.cpp"
+    has_while_file_bad = "test/fixtures/has-forever-while-bad-format.cpp"
 
     def test_lint(self):
-        self.assertEquals(sum(1 for line in open(self.has_while_file, 'rb')),
+        self.assertNotEquals(sum(1 for line in open(self.has_while_file, 'rb')),
                 LoopControlLinter().lint(self.has_while_file))
 
     def test_offenses(self):
@@ -16,6 +17,9 @@ class LoopControlLinterTest(TestCase):
         self.assertEquals(0, l.offenses())
         l.lint(self.has_while_file)
         self.assertEquals(1, l.offenses())
+        another_l = LoopControlLinter()
+        another_l.lint(self.has_while_file_bad)
+        self.assertEquals(l.offenses(), another_l.offenses())
 
 if __name__ == "__main__":
     main()
