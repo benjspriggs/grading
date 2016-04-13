e75bc2ac45ec85f3af9a54dae4a6593b8c4e1b59
Benjamin Spriggs <benjspriggs@sprico.com>
2016-04-13 15:52:48 -0700

    Add rest of manual check to fragment

--- fragments/manual-check.sh
+++ fragments/manual-check.sh
@@ -2,3 +2,12 @@
 # fragments/manual-check.sh
 # Manually open up all .h and .cpp files
 # in the current directory
+
+# TODO add some way to process comment density?
+if ls -l *.h > /dev/null 2>&1;then
+  vim -p *.h $STUDENT_REPORT
+fi
+if ls -l *.cpp > /dev/null 2>&1; then
+  vim -p *.cpp $STUDENT_REPORT
+fi
+
