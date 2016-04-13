8feea6d6716d37d5a38116d5c2a53251a06ced37
Benjamin Spriggs <benjspriggs@sprico.com>
2016-04-13 15:46:24 -0700

    Add leak-check fragment

---
+++ fragments/leak-check.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# fragments/leak-check.sh
+# Makes sure a program has no leaks
+# based on the first argument given (executable name)
+# Errors are dumped into a valgrind_output.txt,
+# then appended to a student's report
+
