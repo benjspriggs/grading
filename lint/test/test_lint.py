9f196c993c6faeb18e2cc0c09d83d072a94c564e
Benjamin Spriggs <benjspriggs@sprico.com>
2016-04-13 10:49:57 -0700

    Remove implementation error on calling unimplemented report in Linter

--- lint/test/test_lint.py
+++ lint/test/test_lint.py
@@ -27,10 +27,6 @@ class TestLinter(unittest.TestCase):
         with self.assertRaises(NotImplementedError):
             TestLinter.lint.offenses()
 
-    def test_report(self):
-        with self.assertRaises(NotImplementedError):
-            TestLinter.lint.report()
-
     def test_number_and_lines(self):
         self.assertNotEquals(
                 len(Linter.number_and_line(self.well_formatted)), 0)
