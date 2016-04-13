d11fcc5f2f355b3e14bd811bfec42ae7011302d4
Benjamin Spriggs <benjspriggs@sprico.com>
2016-04-13 15:40:29 -0700

    Replace help with help fragment

--- fragments/help.sh
+++ fragments/help.sh
@@ -2,4 +2,11 @@
 # fragments/help.sh
 # Display the string in HELP_MSG if --help or -h is in the arguments
 
+# display help message if requested
+for arg in $@; do
+  if [ [$arg] == ["--help"] -o [$arg] == ["-h"] ]; then
+    echo "$HELP_MSG"
+    exit 0
+  fi
+done
 
