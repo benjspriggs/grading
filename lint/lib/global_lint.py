ebaa49e951da9869830b47488d8698f6b18caab3
Benjamin Spriggs <benjspriggs@sprico.com>
2016-04-22 11:02:57 -0700

    Add todo

--- lint/lib/global_lint.py
+++ lint/lib/global_lint.py
@@ -5,11 +5,13 @@
 from lint import *
 
 class GlobalVariableLinter(Linter):
-    has_var = re.compile(r"\b(?:(?:auto\s*|const\s*|unsigned\s*|signed\s*|register\s*|volatile\s*|static\s*|void\s*|short\s*|long\s*|char\s*|int\s*|float\s*|double\s*|_Bool\s*|complex\s*)+)(?:\s+\*?\*?\s*)([a-zA-Z_][a-zA-Z0-9_]*)\s*[\[;,=)]")
+    has_var = re.compile(r"\b(?:(?:auto\s*|const\s*|unsigned\s*|signed\s*|register\s*|volatile\s*|static\s*|void\s*|short\s*|long\s*|char\s*|int\s*|float\s*|double\s*|_Bool\s*|complex\s*)+)(?:\s+\*?\*?\s*)([a-zA-Z_][a-zA-Z0-9_]*)\s*[\[;,=)]") # TODO: This doesn't cover typedefs
+
+    # TODO: HOW????
     def lint(self, fn):
         if Linter.is_lintable(fn):
             file_and_lines = Linter.parseable_lines(fn)
             count = len(Linter.parseable_lines(fn))
-            self.offense_list[str(fn)] = file_and_lines
+            self.offense_list[str(fn)] = filter(lambda (n, l): Linter.has(GlobalVariableLinter.has_var, l), file_and_lines)
             return count 
         return 0
