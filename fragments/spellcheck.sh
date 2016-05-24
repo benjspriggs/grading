edacf401500f34b17d961e1e21dfc39326d011f8
bspriggs on ada <bspriggs@pdx.edu>
2016-05-24 14:30:34 -0700

    Add skeleton for a spellcheck

---
+++ fragments/spellcheck.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+# fragments/spellcheck.sh
+# Spellcheck a bunch of files
+
+# make sure we have the student report
+
+# make sure we have the files to spellcheck
+# (and that they're spellcheckable)
+
+# check a .cpp file...
+# aspell -t check --mode=ccpp --run-together $file.cpp
