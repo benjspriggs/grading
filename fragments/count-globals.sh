cd14d056745cd08b19cb2cfd2771d3ede8b20afd
Benjamin Spriggs <benjspriggs@sprico.com>
2016-05-24 12:54:20 -0700

    Add fragment to count globals, skeleton

---
+++ fragments/count-globals.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# fragments/count-globals.sh
+# Count the number of global variables in every file
+# Dumps everything into the student report as a 'global report'
+
+# make sure we have the student's report
+
+# make sure we have some files to lint
+
+
