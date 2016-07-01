e7034c4a67356f20b076a68fe34ed16533d93dd6
Benjamin Spriggs <benjspriggs@sprico.com>
2016-06-30 21:54:19 -0700

    Move help message

--- grade.sh
+++ grade.sh
@@ -4,7 +4,7 @@
 
 source fragments/common.sh
 
-usage="grade <class> <name>"
+usage="$0 <class> <name>"
 # usage block
 {
   if [ -z "$1" ]; then
@@ -15,7 +15,23 @@ usage="grade <class> <name>"
   fi
 }
 
-class="$1"
-name="$2"
+local class="$1"
+local name="$2"
+
+local GRADING_HOME=${SCRIPT_SOURCE%/*}
+local HELP_MSG="Usage: $usage
+This script takes a student's folder name and runs through automated grading operations for PSU $class
+It will dump out a text file with:
+  the student's name
+  any compile errors
+  other notes
+in the current working directory ($PWD)."
+
+for arg in $@; do
+  if [ [$arg] == ["--help"] -o [$arg] == ["-h"] ]; then
+    echo "$HELP_MSG"
+    exit 0
+  fi
+done
 
 source "$class"/grade.sh "$name"
