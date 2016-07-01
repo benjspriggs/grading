b4d0b334b2b92e2d2ffc1c12d29a6dd5ea9b0026
Benjamin Spriggs <benjspriggs@sprico.com>
2016-06-30 21:20:34 -0700

    Change usage of 162 to function

--- cs162-grade.sh
+++ cs162-grade.sh
@@ -4,59 +4,60 @@
 # Runs through a series of checks, and puts output to a file
 # author :: Benjamin Spriggs
 
-WORKING_DIRECTORY=$(pwd)
-HELP_MSG="Usage: grade [student-dir] [--help]
-This script takes a student's folder name and runs through automated grading operations for PSU CS162
-It will dump out a text file with:
-  the student's name
-  any compile errors
-  other notes
-in the current working directory ($WORKING_DIRECTORY)."
 
-source $GRADING_HOME/fragments/help.sh
-display_help_and_exit
+grade()
+{
+  local WORKING_DIRECTORY=$(pwd)
+  local HELP_MSG="Usage: grade [student-dir]
+  This script takes a student's folder name and runs through automated grading operations for PSU CS162
+  It will dump out a text file with:
+    the student's name
+    any compile errors
+    other notes
+  in the current working directory ($WORKING_DIRECTORY)."
 
-# make sure we have all the arguments
-if [[ -z "$1" || ! -d "$1" ]]; then
-  echo "$HELP_MSG"
-  exit 1
-fi
+  # usage block
+  {
+    if [[ -z "$1" || ! -d "$1" ]]; then
+      die "Missing dirname or non-direcory given" $HELP_MSG
+    fi
+  }
 
-STUDENT_NAME=$1
-STUDENT_REPORT=$WORKING_DIRECTORY/${STUDENT_NAME%/}.txt
+  local STUDENT_NAME="$1"
+  local STUDENT_REPORT=$WORKING_DIRECTORY/${STUDENT_NAME%/}.txt
 
-if [ ! -d $STUDENT_NAME ]; then
-  echo "Directory '$STUDENT_NAME' does not exist."
-  echo "$HELP_MSG"
-  exit 1
-fi
+  if [ ! -d $STUDENT_NAME ]; then
+    echo "Directory '$STUDENT_NAME' does not exist."
+    echo "$HELP_MSG"
+    exit 1
+  fi
 
-## Program must compile
-source $GRADING_HOME/fragments/compile.sh
-compile_strict "$STUDENT_NAME" "$STUDENT_REPORT" a.out
+  ## Program must compile
+  source $GRADING_HOME/fragments/compile.sh
+  compile_strict "$STUDENT_NAME" "$STUDENT_REPORT" a.out
 
-## Program must not have any run-time faults
-source $GRADING_HOME/fragments/no-runtime-errors.sh a.out
-## Functions must be fewer than 30 lines of code
-# TODO Add line checker
+  ## Program must not have any run-time faults
+  source $GRADING_HOME/fragments/no-runtime-errors.sh a.out
+  ## Functions must be fewer than 30 lines of code
+  # TODO Add line checker
 
-# Check for code requirements
-LIB_DIR=$GRADING_HOME/lint
-echo "Checking obvious code errors..."
-python $LIB_DIR/cs162_code.py *.h *.cpp | tee -a $STUDENT_REPORT
-# check for globals and such
-# TODO: Find a better way to find all of the .cpp files
-source $GRADING_HOME/fragments/count-globals.sh
-count_globals $STUDENT_REPORT *.cpp
+  # Check for code requirements
+  LIB_DIR=$GRADING_HOME/lint
+  echo "Checking obvious code errors..."
+  python $LIB_DIR/cs162_code.py *.h *.cpp | tee -a $STUDENT_REPORT
+  # check for globals and such
+  # TODO: Find a better way to find all of the .cpp files
+  source $GRADING_HOME/fragments/count-globals.sh
+  count_globals $STUDENT_REPORT *.cpp
 
-# open up all of their files in vim to check for formatting and add any additional notes
-source $GRADING_HOME/fragments/manual-check.sh
+  # open up all of their files in vim to check for formatting and add any additional notes
+  source $GRADING_HOME/fragments/manual-check.sh
 
-# finish execution
-echo "Anonymizing $STUDENT_REPORT..."
-sed -i -e "s:$(pwd):\.\.\.:g" $STUDENT_REPORT # anonymize the file paths
+  # finish execution
+  echo "Anonymizing $STUDENT_REPORT..."
+  sed -i -e "s:$(pwd):\.\.\.:g" $STUDENT_REPORT # anonymize the file paths
 
-echo "Finished grading ${STUDENT_NAME%/} in $(pwd), returning to grading directory..."
-cd $WORKING_DIRECTORY
-echo "Done."
-
+  echo "Finished grading ${STUDENT_NAME%/} in $(pwd), returning to grading directory..."
+  cd $WORKING_DIRECTORY
+  echo "Done."
+}
