3830022acacf13212c66a0adc661419a76b713f6
Benjamin Spriggs <bspriggs@pdx.edu>
2016-12-10 21:26:54 -0800

    add options to move to locals, no-clean etc

--- move.sh
+++ move.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 # An automated moving and makefile-adding script for CS202
+# usage: ./move.sh [-h] [-l (local dir)] [-n] name archive
+# -h Display help text
+# -l Local directory
+# -n No clean
 
-HELP_MSG="Usage: move (--help) name [archive]
+HELP_MSG="Usage: ./move.sh [-h] [-l (local dir)] [-n] name archive
 This script takes an archive, opens it, adds a default makefile for general use,
 and makes the extracted files available on the PSU linux systems."
 
@@ -16,8 +20,6 @@ DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 PATHS=${DIR%$(basename "$SOURCE")}/paths.sh
 source "$PATHS"
 if [ -z ${GRADING_REMOTE+x} ] || [ -z ${LINUX+x} ] || [ -z ${MAKEFILE+x} ] ; then
-  echo "$SOURCE"
-  echo "$DIR"
   echo "Please install the path script ('path.sh') to $PATHS and update the following paths:
   GRADING_REMOTE
   LINUX
@@ -25,10 +27,27 @@ if [ -z ${GRADING_REMOTE+x} ] || [ -z ${LINUX+x} ] || [ -z ${MAKEFILE+x} ] ; the
   exit 1
 fi
 
+# parse some options
+while getopts hl:n flag; do
+  case $flag in
+    h) 
+      echo -e "$HELP_MSG"
+      exit 0
+      ;;
+    l)
+      local_dir="$( realpath ${OPTARG} )"
+      ;;
+    n) no_clean='true'
+      ;;
+    :) echo "got: $OPTARG";;
+  esac
+done
+shift $((OPTIND - 1))
+
 # check for a help message in arguments
 if [ -z "$1" -o -z "$2" ]; then
   echo "$HELP_MSG"
-  exit 0
+  exit 1
 fi
 
 for arg in $@; do
@@ -69,9 +88,32 @@ go_to_dir_with_cpp() {
   done
 }
 
-if [[ -e "$MAKEFILE" && -e "$ARCHIVE" ]]; then
+# validate the archive and makefile
+_validate_archive_makefile(){
+  if [[ -e "$ARCHIVE" && ! -e "$MAKEFILE" ]]; then
+    echo "Makefile doesn't exist. Add a makefile in $MAKEFILE."
+    exit 1
+  elif [[ -e "$MAKEFILE"  && ! -e "$ARCHIVE" ]]; then
+    echo "Archive \"$ARCHIVE\" doesn't exist."
+    exit 1
+  fi
+}
+
+# validate dir
+_valid_destination(){
+  echo "local dir is: ${local_dir}"
+	if [ ! -z "${local_dir}" ];
+    then
+      return 0
+  else
+    return 1
+  fi
+}
+
+# extract 
+_extract_archive(){
+  # create the archive if it doesn't exist
   [ -d "$NAME" ] || mkdir "$NAME"
-  # extract the archive to a folder
   echo "Extracting archive $ARCHIVE..."
   if [[ "$ARCHIVE" =~ \.zip$ ]]; then
     unzip "$ARCHIVE" -d "$NAME"
@@ -84,22 +126,34 @@ if [[ -e "$MAKEFILE" && -e "$ARCHIVE" ]]; then
     rm -d "$NAME"
     exit 1
   fi
-  # add a makefile to the folder
-  put_makefile_in_dir
+}
 
-  # copy the folder to the linux directory
-  echo Moving $NAME\... in $CWD
-  if $(scp -r "$CWD"/"$NAME" $LINUX:$GRADING_REMOTE); then
-    echo $NAME moved successfully to PSU Linux systems at $GRADING_REMOTE.
-    echo Cleaning up the directory...
-    rm -rf "$NAME"
-    echo Done.
-  fi
-elif [[ -e "$ARCHIVE" ]]; then
-  echo "Makefile doesn't exist. Add a makefile in $MAKEFILE."
-  exit 1
-elif [[ -e "$MAKEFILE" ]]; then
-  echo "Archive \"$ARCHIVE\" doesn't exist."
-  exit 1
+_validate_archive_makefile
+
+# extract the archive to a folder
+_extract_archive
+
+# add a makefile to the folder
+put_makefile_in_dir
+
+# copy the folder to the linux directory,
+# or destination directory
+echo Moving $NAME\...
+
+if _valid_destination; then
+  # move it the local destinaion
+  [ -d "$local_dir" ] || mkdir "$local_dir"
+  mv "$NAME" "$local_dir"
+  echo "$NAME moved successfully to local dir '$local_dir'"
+elif scp -r "$CWD"/"$NAME" $LINUX:$GRADING_REMOTE; then
+  echo $NAME moved successfully to PSU Linux systems at $GRADING_REMOTE.
 fi
 
+if [ ! -z "$no_clean" ]; then 
+  exit 0 
+fi
+
+# clean up
+echo Cleaning up the directory...
+rm -rf "$NAME"
+echo Done.
